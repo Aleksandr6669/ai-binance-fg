c6515a82a21ae105966d333c91aa29151c4f9071
Aleksandr6669 <Gabreil666@gmail.com>
2026-08-05 20:11:08 +0300

    Cleanup temporary script files

--- test_session.py
+++
@@ -1,5 +0,0 @@
-import urllib.parse
-query_string = "sessionId=12345&foo=bar"
-parsed_query = urllib.parse.parse_qs(query_string)
-if "sessionId" in parsed_query:
-    print(parsed_query["sessionId"][0])
